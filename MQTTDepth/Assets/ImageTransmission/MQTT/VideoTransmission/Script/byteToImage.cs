adea78ab8d4f7141a48f2c7081ab821fa35c687f
jieguann <60665347+jieguann@users.noreply.github.com>
2021-02-02 21:41:53 -0500

    Update byteToImage.cs

--- MQTTDepth/Assets/ImageTransmission/MQTT/VideoTransmission/Script/byteToImage.cs
+++ MQTTDepth/Assets/ImageTransmission/MQTT/VideoTransmission/Script/byteToImage.cs
@@ -21,7 +21,7 @@ public class byteToImage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        receiveBytes = mqtt.receiveByte;
+        receiveBytes = mqtt.depthByte;
 
         if(receiveBytes != null)
         {
